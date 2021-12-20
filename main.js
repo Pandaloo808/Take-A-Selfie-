76453344c71d0eb594f4e5b73da41a7b90f47b4f
Pandaloo808 <93350605+Pandaloo808@users.noreply.github.com>
2021-12-20 16:44:54 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,46 @@
 var SpeechRecognition=window.webkitSpeechRecognition;
 var recognition=new SpeechRecognition();
+var Textbox=document.getEelementById("textbox");
 function start(){
-  document.getElementById("textbox").innerHTML="";
+  Textbox.innerHTML="";
   recognition.start();
 }
-recognition.onresult=function run(event){
+recognition.onresult=function(event){
+  console.log(event);
   var Content=event.results[0][0].transcript;
-  document.getElementById("textbox").innerHTML=Content;
+  Textbox.innerHTML=Content;
   console.log(Content);
-  speak();
+  if(Content=="Take My Selfie"){
+    console.log("taking your selfie-");
+    speak(); 
+  }
 }
 function speak(){
   var synth=window.speechSynthesis;
-  speak_data=document.getElementById("textbox").value;
+  speak_data="Taking your selfie in 5 seconds";
   var utterThis=new SpeechSynthesisUtterance(speak_data);
-  synth.peek(utterThis);
+  synth.speak(utterThis);
   Webcam.attach(camera);
+  setTimeout(function(){
+    take_selfie();
+    save();
+    },5000);
 }
+camera=document.getElementById("camera");
 Webcam.set({
   width:360,
   height:250,
   image_format:'png',
-  png_quality:90
+  png_quality:100
 });
-camera=document.getElementById("camera");
+function take_selfie(){
+Webcam.snap(function(data_ur{i)
+document.getElementById("result").innerHTML='<img id="selfie_image" src="'+data_uri+'"/>';
+});
+}
+function save(){
+link=document.getElementById("link");
+  image=document.getElementById("selfie_image").src;
+  link.href=image;
+  link.click();
+}
